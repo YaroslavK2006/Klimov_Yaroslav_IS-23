3549e385feca48873e131e6a3a2ad24335dbbd8f
posovestii <kimyana2007@gmail.com>
2023-12-14 11:30:15 +0300

    add PZ_4

--- PZ6/PZ_6_2.py
+++ PZ6/PZ_6_2.py
@@ -1,11 +1,11 @@
 def find_closest_value(R, A):
-    closest_value = None  # Переменная closest_value будет использоваться для хранения ближайшего значения
+    closest_value = None
     min_difference = float('inf')  # Переменной min_difference со значением бесконечности эта переменная будет содержать наименьшую разницу между текущим числом из списка и числом R
 
     for num in A:  # Цикл for который перебирает каждое число num из списка A
         difference = abs( num - R)  # Вычисляет разницу между числом num и значением R с помощью функции abs которая возвращает абсолютное значение
         if difference < min_difference:
-            min_difference = difference  # Значение переменной min_difference обновляется на текущую разницу difference потому что она является новым наименьшим значением разницы
+            min_difference = difference
             closest_value = num
 
     return closest_value  # Возврат значения переменной closest_value из функции
