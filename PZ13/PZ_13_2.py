6750ad28cbde06fc89652780b1e097a8f47d958b
posovestii <kimyana2007@gmail.com>
2024-03-27 08:38:19 +0300

    add PZ_9

--- PZ13/PZ_13_2.py
+++ PZ13/PZ_13_2.py
@@ -1,18 +1,20 @@
 # В матрице найти сумму элементов второй половины матрицы.
 def sum_second_half(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
+    rows = len(matrix) # # Количество строк в матрице
+    cols = len(matrix[0]) # # Количество столбцов в матрице
 
     if rows % 2 != 0:
         return "Матрица должна иметь четное количество строк"
 
-    half_rows = rows // 2
-    second_half_sum = 0
+    half_rows = rows // 2 # Индекс строки с которой начинается вторая половина матрицы
+    second_half_sum = 0 # Переменная для хранения суммы элементов второй половины матрицы
 
+ #  Считаем сумму элементов второй половины матрицы
     for i in range(half_rows, rows):
         for j in range(cols):
             second_half_sum += matrix[i][j]
 
+    # Возвращаем сумму элементов второй половины матрицы
     return second_half_sum
 
 
@@ -23,6 +25,6 @@ matrix = [
     [7, 8, 9],
     [10, 11, 12]
 ]
-
+# Вызываем функцию и выводим результат
 result = sum_second_half(matrix)
 print("Сумма элементов второй половины матрицы:", result)
