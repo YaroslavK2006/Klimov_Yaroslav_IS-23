adb317f27bc849040e02a5e3b9be1310a89b0379
Admin <yaroslav.klimov.06@bk.ru>
2024-06-23 22:51:19 +0300

    add PZ_9

--- PZ17/17_2.py
+++ PZ17/17_2.py
@@ -6,8 +6,8 @@ from tkinter import messagebox
 def check_number():
     try:
         number = int(entry.get())
-        if 1 <= number <= 999:
-            if number % 2 == 0:
+        if 1 <= number <= 999: # Проверяем, находится ли число в диапазоне от 1 до 999
+            if number % 2 == 0: # Проверяем, является ли число четным
                 if 1 <= number < 10:
                     result = "Четное однозначное число"
                 elif 10 <= number < 100:
@@ -27,16 +27,16 @@ def check_number():
         result = "Введено не целое число"
     messagebox.showinfo("Результат", result)
 
-root = tk.Tk()
+root = tk.Tk() # Создаем новое окно tkinter
 root.title("Проверка четности числа")
 
-label = tk.Label(root, text="Введите целое число от 1 до 999:")
-label.pack()
+label = tk.Label(root, text="Введите целое число от 1 до 999:") # Создаем метку с указанным текстом
+label.pack() # Добавляем метку в окно
 
-entry = tk.Entry(root)
+entry = tk.Entry(root) # # Создаем поле ввода
 entry.pack()
 
-button = tk.Button(root, text="Проверить", command=check_number)
+button = tk.Button(root, text="Проверить", command=check_number) # Создаем кнопку с указанным текстом и связываем ее с функцией check_number
 button.pack()
 
 root.mainloop()
